4cf0a895939580427e4ab62a3895749df788744a
Richard Kenny <rj.kenny@hotmail.com>
2016-01-02 19:22:56 -0500

    Ready to take a break before adding command reduction

--- src/main/java/tk/bad_rabbit/rcam/distributed_backend/command/responseactions/ResultCommandResponseAction.java
+++ src/main/java/tk/bad_rabbit/rcam/distributed_backend/command/responseactions/ResultCommandResponseAction.java
@@ -4,6 +4,8 @@ import java.util.Observer;
 
 import tk.bad_rabbit.rcam.distributed_backend.command.ACommand;
 import tk.bad_rabbit.rcam.distributed_backend.command.states.CommandReadyToReduceState;
+import tk.bad_rabbit.rcam.distributed_backend.command.states.DoneState;
+import tk.bad_rabbit.rcam.spring.commands.CommandController;
 
 public class ResultCommandResponseAction extends ACommandResponseAction {
 
@@ -20,13 +22,15 @@ public class ResultCommandResponseAction extends ACommandResponseAction {
   @Override
   public void doStuff(Observer actionObject, String server, ACommand actionSubject) {
     System.out.println("If the command was successful, the next step will be to check if the command is ready to reduce.");
+    
+    ((CommandController) actionObject).getCommand((Integer) actionSubject.getClientVariable("ackNumber")).setState(server, new CommandReadyToReduceState());
     nextState(server, actionSubject);
   }
 
   
   
   public void nextState(String server, ACommand command) {
-    command.setState(server, new CommandReadyToReduceState());
+    command.setState(server, new DoneState());
   }
 
   
